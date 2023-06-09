a8c2f767f0576d07df2f8a52d4b22beb93aee352
unknown <owasp.pe@gmail.com>
2023-06-09 02:22:19 -0500

    Actualizacion de Controlador 1

--- src/main/java/pe/gob/indeci/sinpad/controlador/ControladorParametro.java
+++ src/main/java/pe/gob/indeci/sinpad/controlador/ControladorParametro.java
@@ -1,13 +1,17 @@
 package pe.gob.indeci.sinpad.controlador;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,9 +40,17 @@ public class ControladorParametro {
 		 servicio.delete(id);
 	}
 	
-	@PostMapping("/guardar/{p}")
-	public Parametro guardar(@PathVariable Parametro p) {
-		return servicio.save(p);
+	
+	@PostMapping(
+	        value = "/guardar",
+	        consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+	        produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})   
+	public ResponseEntity<Parametro> guardar(@RequestBody Parametro p) {
+		Parametro parametroPersistente = servicio.save(p);
+        return ResponseEntity
+            .created(URI
+                     .create(String.format("/mostrar/%s", parametroPersistente.getId())))
+            .body(parametroPersistente);
 	}
 	
 	@PutMapping("/actualizar/{p}")
